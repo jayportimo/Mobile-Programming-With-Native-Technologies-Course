42ca348670ac542d28deeedf1b5875781c6d034a
Johanna Portimo <71264870+jayportimo@users.noreply.github.com>
2024-02-26 23:17:16 +0200

    Initial creation of loginform

---
+++ LoginForm/app/src/main/java/com/example/loginform/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.loginform
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.loginform.ui.theme.LoginFormTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginFormTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Login()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Login() {
+    var userName: String by remember { mutableStateOf("") }
+    var passwordVisualTransformation: String by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.login),
+            fontSize = 24.sp,
+            color = MaterialTheme.colors.primary,
+            textAlign = TextAlign.Left,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp)
+        )
+        OutlinedTextField(
+            value = userName,
+            trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = "emailIcon") },
+            onValueChange = { /*TODO*/ },
+            label = { Text(text = stringResource(R.string.username))},
+            placeholder = { Text(text = "example@email.com") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = passwordVisualTransformation,
+            trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = "lockIcon") },
+            onValueChange = { passwordVisualTransformation = it },
+            label = { Text(text = stringResource(R.string.password))},
+            placeholder = { Text(text = "**********") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.submit))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoginFormTheme {
+        Login()
+    }
+
+}
