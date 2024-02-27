31ea906bad2b69d220846efbb7b5dd96d959cb71
Johanna Portimo <71264870+jayportimo@users.noreply.github.com>
2024-02-27 17:04:11 +0200

    Remove warning causes

--- LoginForm/app/src/main/java/com/example/loginform/MainActivity.kt
+++ LoginForm/app/src/main/java/com/example/loginform/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
@@ -16,7 +15,8 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,7 +24,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,7 +50,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Login() {
-    var userName: String by remember { mutableStateOf("") }
+    val userName: String by remember { mutableStateOf("") }
     var passwordVisualTransformation: String by remember { mutableStateOf("") }
 
     Column(
@@ -69,18 +68,18 @@ fun Login() {
         )
         OutlinedTextField(
             value = userName,
-            trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = "emailIcon") },
-            onValueChange = { /*TODO*/ },
+            trailingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon") },
             label = { Text(text = stringResource(R.string.username))},
             placeholder = { Text(text = "example@email.com") },
+            onValueChange = { /*TODO*/ },
             modifier = Modifier.fillMaxWidth()
         )
         OutlinedTextField(
             value = passwordVisualTransformation,
-            trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = "lockIcon") },
-            onValueChange = { passwordVisualTransformation = it },
+            trailingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "lockIcon") },
             label = { Text(text = stringResource(R.string.password))},
             placeholder = { Text(text = "**********") },
+            onValueChange = { passwordVisualTransformation = it },
             visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier.fillMaxWidth()
         )
