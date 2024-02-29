1dcdea29264d7bc97bb6edd8179b022b06d6dcd8
Johanna Portimo <71264870+jayportimo@users.noreply.github.com>
2024-02-29 18:28:41 +0200

    Initial commit for scaffoldnavigation app

---
+++ ScaffoldNavigation/app/src/main/java/com/example/scaffoldnavigation/MainActivity.kt
@@ -0,0 +1,137 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.scaffoldnavigation
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.scaffoldnavigation.ui.theme.ScaffoldNavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScaffoldNavigationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ScaffoldApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ScaffoldApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "Home"
+    ) {
+        composable(route = "Home") {
+            MainScreen(navController)
+        }
+        composable(route = "Info") {
+            InfoScreen(navController)
+        }
+        composable(route = "Settings") {
+            SettingsScreen(navController)
+            }
+    }
+}
+
+@Composable
+fun MainTopBar(title: String, navController: NavController) {
+    var expanded by remember { mutableStateOf(false) }
+    TopAppBar(
+        title = { Text(title) },
+        actions = {
+            IconButton(
+                onClick = {
+                    expanded = !expanded
+                }
+            ) {
+                Icon(Icons.Filled.MoreVert,contentDescription = null)
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }) {
+                DropdownMenuItem(text = {Text("Info")}, onClick = { navController.navigate("info") })
+                DropdownMenuItem(text = {Text("Settings")}, onClick = { navController.navigate("settings") })
+            }
+        }
+    )
+}
+
+@Composable
+fun ScreenTopBar(title: String, navController: NavController) {
+    TopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = null)
+            }
+        })
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen(navController: NavController) {
+    Scaffold(
+        topBar = { MainTopBar(title = "My app", navController) },
+        content = { Text(text = "Content for Home screen")},
+    )
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun InfoScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ScreenTopBar(title = "Info", navController )},
+        content = { Text(text = "Content for Info screen")},
+    )
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun SettingsScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ScreenTopBar(title = "Settings", navController )},
+        content = { Text(text = "Content for Settings screen")},
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ScaffoldNavigationTheme {
+        ScaffoldApp()
+    }
+}
